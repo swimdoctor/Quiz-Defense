756ed7f7050922787cea7ce1a00906db8a93e660
swimdoctor <43818006+swimdoctor@users.noreply.github.com>
2022-03-20 15:55:14 -0700

    test :crossed_fingers:

--- docs/card.js
+++ docs/card.js
@@ -1,11 +1,5 @@
 "use strict";
 exports.__esModule = true;
 exports.card = void 0;
-var card = /** @class */ (function () {
-    function card(Term, Definition) {
-        this.Term = Term;
-        this.Definition = Definition;
-    }
-    return card;
-}());
+
 exports.card = card;
