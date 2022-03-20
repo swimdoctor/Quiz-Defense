b725d63bceb5b08673a502e0f1e1b54ff499417f
swimdoctor <43818006+swimdoctor@users.noreply.github.com>
2022-03-20 14:27:29 -0700

    t

--- main.ts
+++ main.ts
@@ -48,7 +48,7 @@ function shuffleArray(array) {
 }
 
 function goToMenu(){
-  document.getElementById("card").style.display = "none"
+  document.getElementById("card").style.width = "500px";
   /*x.style.display = "block";
   x.style.display = "none";*/
 }
@@ -59,6 +59,7 @@ function goToSetCreation(){
 }
 
 function goToPlaySet(){
-  document.getElementsByClassName("card")[0].style.display = "inline_block"
+  document.getElementById("mySidebar").style.width = "500px";
+  document.getElementById("card").style.display = "inline_block"
 
 }
